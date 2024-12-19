e6c24490778ff7b846b1b4ec8d5fb81094d9df1d
Shivam Choudhary <02shivam.choudhary@gmail.com>
2024-12-19 11:56:49 +0530

    Update CSVUpload.jsx

--- quiz-app/src/Admin/CSVUpload.jsx
+++ quiz-app/src/Admin/CSVUpload.jsx
@@ -20,7 +20,7 @@ const CreateCustomExam = () => {
   useEffect(() => {
     const fetchTechnologies = async () => {
       try {
-        const response = await axios.get("http://localhost:8080/api/technologies");
+        const response = await axios.get("https://mcq-project-backend.onrender.com/api/technologies");
         setTechnologies(response.data);
       } catch (error) {
         setErrorMessage("Failed to fetch technologies");
@@ -53,7 +53,7 @@ const CreateCustomExam = () => {
 
     try {
       const response = await axios.post(
-        'http://localhost:8080/api/questions/upload-questions',
+        'https://mcq-project-backend.onrender.com/api/questions/upload-questions',
         formData,
         {
           headers: {
@@ -99,7 +99,7 @@ const CreateCustomExam = () => {
       admin_id: admin_id,
     };
     try {
-      await axios.post("http://localhost:8080/api/customexams/create", payload);
+      await axios.post("https://mcq-project-backend.onrender.com/api/customexams/create", payload);
       setSuccessMessage("Exam created successfully!");
       setErrorMessage("");
       setSelectedTechnology("");
