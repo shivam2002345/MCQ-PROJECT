51048cfa432f1c8cfbb7471874d7b61d371ba202
Shivam Choudhary <02shivam.choudhary@gmail.com>
2024-12-19 12:21:12 +0530

    Update logAction.js

--- quiz-app/src/utils/logAction.js
+++ quiz-app/src/utils/logAction.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const logAction = async (action, details) => {
     try {
-        await axios.post('http://localhost:8080/api/logs', { action, details, timestamp: new Date() });
+        await axios.post('https://mcq-project-backend.onrender.com/api/logs', { action, details, timestamp: new Date() });
     } catch (error) {
         console.error('Failed to log action:', error.message);
     }
